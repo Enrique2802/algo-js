34f07ebca53fc177568ebe9de12baffec06086c7
Enrique Ruard <enriqueruard123@gmail.com>
2025-11-20 10:57:28 +0100

    exercise 2.2.js done

---
+++ 2.2.js
@@ -0,0 +1,11 @@
+let min = Number(prompt("Enter min:"));
+
+undefined
+let max = Number(prompt("Enter max"));
+undefined
+let current = Number(prompt("Enter current"));
+undefined
+if (current < max && current > min) {console.log("Current is between min and max")};
+
+Current is between min and max
+
