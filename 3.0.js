e3f239556e24fe2efd8b3963c0b1eb038f108326
Enrique Ruard <enriqueruard123@gmail.com>
2025-11-21 11:59:02 +0100

    exercise 3.0 done

---
+++ 3.0.js
@@ -0,0 +1,2 @@
+let learners = ["Enrique","Lucas","Ethan","Micael","Dorian","Johan","Abdoul","Jonathan","Matthias","Julien","Lindsay","Isaac","Kev","Thomas","Léandre","Dimitri","Gaëtan"];
+ for (let learner of learners){console.log(learner);}
