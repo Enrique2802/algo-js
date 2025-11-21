4be2db83dcb54053c77bf734b27c224dfe4122ae
Enrique Ruard <enriqueruard123@gmail.com>
2025-11-21 14:07:46 +0100

    exercise 3.2 done

---
+++ 3.2.js
@@ -0,0 +1,21 @@
+let numbers = [1, 2, 3, 4, 5]
+
+let total = 0
+
+for (let n of numbers) { total += n; }
+
+let average = total / numbers.length
+
+average
+
+---
+
+let numbers = [100,101,102]
+
+let total = 0
+
+for (let n of numbers) { total += n; }
+
+let average = total / numbers.length
+
+average
