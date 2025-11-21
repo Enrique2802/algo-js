b6826988d9b5d7cbea0dde1bfa431552421fe027
Enrique Ruard <enriqueruard123@gmail.com>
2025-11-21 10:45:55 +0100

    exercise 2.7 done

---
+++ 2.7.js
@@ -0,0 +1,5 @@
+let n = Number(prompt("How many numbers do you want to enter"));
+ for (let i = 1; i <= n; i++)
+ {let value = Number(prompt("Enter number "+i+" : "));
+ sum = sum + value;}
+alert("The total sum is: " + sum);
